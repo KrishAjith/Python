8577b9268b04a35f22915f56c2ea15eb44ba5910
KrishAjith <ajithkumar1997k@gmail.com>
2017-06-21 14:11:34 +0530

    Create FindLargeNum.py

---
+++ FindLargeNum.py
@@ -0,0 +1,9 @@
+Num1=int(input("Enter your 1st Number     :"))
+Num2=int(input("Enter your 2nd Number     :"))
+Num3=int(input("Enter your 3rd Number     :"))
+if(Num1 > Num2):
+    print(Num1," is Largest Number")
+elif(Num2 > Num3):
+    print(Num2," is Largest Number")
+else:
+    print(Num3," is Largest Number ")
