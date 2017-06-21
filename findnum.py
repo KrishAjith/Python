7e8c9ad2ee90ed077552aa377a2d04113177e04e
KrishAjith <ajithkumar1997k@gmail.com>
2017-06-21 11:15:14 +0530

    Create findnum.py

---
+++ findnum.py
@@ -0,0 +1,8 @@
+Num = int(input("Enter your Number    :   "))
+if( Num != 0):
+    if(Num < 0):
+        print("\n *** Given number is Negative *** \n")
+    else:
+        print("\n *** Given number is Positive ***\n")
+else:
+    print("\n *** Given Number is zero ***\n ")
