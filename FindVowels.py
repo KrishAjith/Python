da3840a71dfa14765773cbcef1d103c1df19b70a
KrishAjith <ajithkumar1997k@gmail.com>
2017-06-21 12:28:59 +0530

    Create FindVowels.py

---
+++ FindVowels.py
@@ -0,0 +1,5 @@
+a=input("Enter your Character     :  ").lower()
+if( a == "a" or a == "e" or a == "i" or a == "o" or a == "u"):
+    print("\n ** Given Character is Vowel ** ")
+else:
+    print("\n ** Given Character is not Vowel ** ")
